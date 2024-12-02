971dfefdb8eade8d9900518605ed7a0126fad16a
cityhonor <user@100ask.com>
2024-12-01 22:53:26 -0500

    feat:更改smp工程

--- src/main.c
+++ src/main.c
@@ -1,74 +1,104 @@
-/* Kernel includes. */
-#include "FreeRTOS.h" /* Must come first. */
-#include "task.h"     /* RTOS task related API prototypes. */
-#include "queue.h"    /* RTOS queue related API prototypes. */
-#include "timers.h"   /* Software timer related API prototypes. */
-#include "semphr.h"   /* Semaphore related API prototypes. */
+/* 
+    Minimal example to use FreeRTOS SMP with task affinity enabled.
+
+    Create TWO very simple tasks and run each on PICOs Cores:
+    - C0 : print message to console and blink an LED
+    - C1 : print message to console
+
+    note : use minicom to observe console messages
+    $ minicom -b 115200 -o -D /dev/ttyACM0
+
+*/
 
 #include <stdio.h>
-#include <stdlib.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
 
 #include "pico/stdlib.h"
+#include "pico/multicore.h"
 
+/* DEFINES */
+#define TASK_DELAY_C0_MS    500
+#define TASK_DELAY_C1_MS    500
+#define DEBUG_AFFINITY      1
 
-static void led_blink (void *paramster)
-{
-    gpio_init(PICO_DEFAULT_LED_PIN);
-    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+/* PRIVATE FUNCTIONS :  headers */
+static void prvSetupHardware(void);
+static void prvTask_C0(void *pvParameters);
+static void prvTask_C1(void *pvParameters);
 
-    while(1)
-    {
-        vTaskDelay(500);
-        gpio_put(PICO_DEFAULT_LED_PIN, 1);
-        vTaskDelay(500);
-        gpio_put(PICO_DEFAULT_LED_PIN, 0);
-    }
+/**/
+/* PUBLIC FUNCTIONS */
+/**/
+
+/* FreeRTOS callbacks */
+void vApplicationMallocFailedHook( void );
+void vApplicationIdleHook( void );
+void vApplicationStackOverflowHook( TaskHandle_t pxTask, char *pcTaskName );
+void vApplicationTickHook( void );
+
+void vApplicationMallocFailedHook(void){
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
+}
+void vApplicationStackOverflowHook(TaskHandle_t pxTask, char *pcTaskName){
+    (void)pxTask;
+    (void)pcTaskName;
+    /* Force an assert. */
+    configASSERT( ( volatile void * ) NULL );
 }
 
-int main(void)
-{
-    /* Configure the system ready to run the demo.  The clock configuration
-    can be done here if it was not done before main() was called. */
-    /* Want to be able to printf */
-    stdio_init_all();
 
-    printf("FreeRTOS SMP Template for Raspberry Pi Pico Board\n");
+/** Creates tasks and allocates to a particular PICO core : C0,C1 */
+int main(){
+    TaskHandle_t xHandle[2];
+    
+    prvSetupHardware();
 
-    /* Create a simple example task */
-    xTaskCreate(led_blink, "led", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+    /* Create Tasks */
+    xTaskCreate( prvTask_C0, "C0", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1 , &(xHandle[0]) );
+    xTaskCreate( prvTask_C1, "C1", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1 , &(xHandle[1]) );
 
-    /* Start the tasks and timer running. */
+#if DEBUG_AFFINITY == 1
+    vTaskCoreAffinitySet(xHandle[0],(UBaseType_t)(1<<0));
+    vTaskCoreAffinitySet(xHandle[1],(UBaseType_t)(1<<1));
+#endif
+
+    /* start scheduler */
+    printf("starting scheduler on Core : %d",get_core_num());
     vTaskStartScheduler();
 
-    /* should never reach here */
-    panic_unsupported();
 }
-/*-----------------------------------------------------------*/
 
-void vApplicationMallocFailedHook( void )
-{
-    /* The malloc failed hook is enabled by setting
-    configUSE_MALLOC_FAILED_HOOK to 1 in FreeRTOSConfig.h.
+/**/
+/* PRIVATE FUNCTIONS : implementation */
+/**/
 
-    Called if a call to pvPortMalloc() fails because there is insufficient
-    free memory available in the FreeRTOS heap.  pvPortMalloc() is called
-    internally by FreeRTOS API functions that create tasks, queues, software
-    timers, and semaphores.  The size of the FreeRTOS heap is set by the
-    configTOTAL_HEAP_SIZE configuration constant in FreeRTOSConfig.h. */
-    panic("malloc failed");
+/** Initialize PICO's required HW */
+static void prvSetupHardware(void){
+    stdio_init_all();
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN,GPIO_OUT);
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
 }
-/*-----------------------------------------------------------*/
 
-void vApplicationStackOverflowHook( TaskHandle_t xTask, char *pcTaskName )
-{
-    ( void ) pcTaskName;
-    ( void ) xTask;
-
-    /* Run time stack overflow checking is performed if
-    configconfigCHECK_FOR_STACK_OVERFLOW is defined to 1 or 2.  This hook
-    function is called if a stack overflow is detected.  pxCurrentTCB can be
-    inspected in the debugger if the task name passed into this function is
-    corrupt. */
-    for( ;; );
+/** run something on C0 : print to console and blink an Led */
+static void prvTask_C0(void *pvParameters){
+    
+    while(1){
+        printf("Task C0 on %d\n", get_core_num());
+        gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        vTaskDelay(TASK_DELAY_C0_MS);
+        gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        vTaskDelay(TASK_DELAY_C0_MS);
+    }
 }
-/*-----------------------------------------------------------*/
+/** run something on C1 : print to console */
+static void prvTask_C1(void *pvParameters){
+    while(1){
+        printf("Task C1 on %d\n", get_core_num());
+        vTaskDelay(TASK_DELAY_C1_MS);
+    }
+}
